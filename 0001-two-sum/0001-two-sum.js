8efeec078697fe22fa0460a97fdda05f5a561c74
Hasan mahmood <139010655+HasanMahmood4U@users.noreply.github.com>
2026-06-30 01:36:03 +0530

    Time: 32 ms (35.5%), Space: 54.5 MB (70.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,9 +1,9 @@
-function twoSum(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
+var twoSum = function(nums, target) {
+    for(var i=0;i<nums.length;i++){
+        for(var j = i+1;j<nums.length;j++){
+            if(nums[i]+nums[j] == target){
+                return [i,j]
             }
         }
     }
-}
+};
